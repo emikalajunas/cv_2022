ef1dc2b6cf371c258b85eb36ae29fb26efe0c584
Edvinas Mikalajunas <edvinas.mikalajunas@gmail.com>
2022-10-03 16:28:14 +0300

    7: working on config.php

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+//database constants localhost
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_NAME','cv_2022_db');
+
+//database constants remote
+//define('DB_HOST','localhost');
+//define('DB_USER','addparklt_cvdb');
+//define('DB_PASSWORD','fwfewf55665hwd!@2ddf');
+//define('DB_NAME','addparklt_cvdb');
+
+?>
